b32eb4d89bd3b951dfd0d43fe5baa15f626fadb0
choseohye <hyesaye@icloud.com>
2025-05-20 17:25:50 +0900

    CROSS ORIGIN

--- src/main/java/com/withcare/member/controller/LoginController.java
+++ src/main/java/com/withcare/member/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +23,7 @@ import com.withcare.member.service.LoginService;
 
 import com.withcare.util.JwtToken.JwtUtils;
 
+@CrossOrigin
 @RestController
 public class LoginController {
 	
