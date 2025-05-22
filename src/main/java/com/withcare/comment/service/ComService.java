cb286467b4465865c74ced33534894037a8582d9
oneidsin <oneidsin@naver.com>
2025-05-22 14:48:11 +0900

    댓글 작성 알림 테이블 자동 추가

--- src/main/java/com/withcare/comment/service/ComService.java
+++ src/main/java/com/withcare/comment/service/ComService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.withcare.noti.service.NotiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,35 +20,49 @@ import com.withcare.comment.dto.MenDTO;
 @Service
 public class ComService {
 
-	Logger log = LoggerFactory.getLogger(getClass());
+    Logger log = LoggerFactory.getLogger(getClass());
 
-	Map<String, Object> result = null;
+    Map<String, Object> result = null;
 
-	@Autowired ComDAO dao;
+    @Autowired
+    ComDAO dao;
 
-	// WRITE COMMENT
+    @Autowired
+    NotiService notiService;
 
-	public Map<String, Object> writeCom(ComDTO dto) {
-		
-		result = new HashMap<>();
+    // WRITE COMMENT
 
-		// 댓글 작성
-		int row = dao.writeCom(dto);
-		
-		if (row == 0) {
-	        result.put("success", false);
-	        result.put("mentioned", new HashSet<String>());
-	        
-	        return result;
-	    }
-		
-		// 댓글 내용에서 멘션된 아이디 조회
-		
-		String content = dto.getCom_content();
-		Set<String> mentionId = getId(content); // 아이디 갖고 올거에요
-		
-		// 멘션 정보 저장할 것
-		for (String menId : mentionId) {
+    public Map<String, Object> writeCom(ComDTO dto) {
+
+        result = new HashMap<>();
+
+        // 댓글 작성
+        int row = dao.writeCom(dto);
+        if (row > 0) {
+            // 댓글 작성 성공 시 알림 저장
+            int com_idx = dto.getCom_idx();
+            int post_idx = dto.getPost_idx();
+            String writer_id = dto.getId();
+            String post_writer_id = dao.getPostWriterId(dto.getPost_idx());
+            if (!writer_id.equals(post_writer_id)) { // 본인 댓글은 알림 제외
+                notiService.sendCommentNoti(com_idx, post_idx, writer_id, post_writer_id);
+            }
+            result.put("success", true);
+            return result;
+        }
+        if (row == 0) {
+            result.put("success", false);
+            result.put("mentioned", new HashSet<String>());
+
+            return result;
+        }
+
+        // 댓글 내용에서 멘션된 아이디 조회
+        String content = dto.getCom_content();
+        Set<String> mentionId = getId(content); // 아이디 갖고 올거에요
+
+        // 멘션 정보 저장할 것
+        for (String menId : mentionId) {
             MenDTO menDto = new MenDTO();
             menDto.setCom_idx(dto.getCom_idx()); // 어떤 댓글에서 멘션 했는지
             menDto.setMen_id(menId); // 멘션 당한 사람 아이디
@@ -58,17 +73,16 @@ public class ComService {
             dao.writeMention(menDto); // DB에 저장
         }
 
-		result.put("success", true);
-	    result.put("idx", dto.getCom_idx());
-	    result.put("mentioned", mentionId);
+        result.put("success", true);
+        result.put("idx", dto.getCom_idx());
+        result.put("mentioned", mentionId);
 
-	    return result;
-        
-	}
-	
-	// GET MENTIONED ID
-	
-	private Set<String> getId (String content) {
+        return result;
+
+    }
+
+    // GET MENTIONED ID
+    private Set<String> getId(String content) {
         List<String> memberIdList = dao.selectId(); // DB에서 멘션된 아이디 가져오기
         Set<String> mentioned = new HashSet<>(); // 멘션된 아이디 저장할 set
 
@@ -81,58 +95,54 @@ public class ComService {
         return mentioned; // 멘션된 아이디 반환
     }
 
-	// UPDATE COMMENT
+    // UPDATE COMMENT
+    public boolean updateCom(ComDTO dto, String id) {
 
-	public boolean updateCom(ComDTO dto, String id) {
-		
-		String writerId = dao.writerId(dto.getCom_idx());
-		
-		if (!writerId.equals(id)) {
-			int lv_idx = dao.userLevel(id);
-			if (lv_idx != 7) {
-				return false;
-			}
-		}
+        String writerId = dao.writerId(dto.getCom_idx());
 
-		int row = dao.updateCom(dto);
-		return row > 0;
-	}
+        if (!writerId.equals(id)) {
+            int lv_idx = dao.userLevel(id);
+            if (lv_idx != 7) {
+                return false;
+            }
+        }
 
-	// DELETE COMMENT
+        int row = dao.updateCom(dto);
+        return row > 0;
+    }
 
-	public boolean delCom(ComDTO dto, String id) {
+    // DELETE COMMENT
+    public boolean delCom(ComDTO dto, String id) {
 
-		String writerId = dao.writerId(dto.getCom_idx());
-		if (id == null) {
-			return false;
-		}
+        String writerId = dao.writerId(dto.getCom_idx());
+        if (id == null) {
+            return false;
+        }
 
-		if (!writerId.equals(id)) {
-			int lv_idx = dao.userLevel(id);
-			if (lv_idx != 7) {
-				return false;
-			}
-		}
+        if (!writerId.equals(id)) {
+            int lv_idx = dao.userLevel(id);
+            if (lv_idx != 7) {
+                return false;
+            }
+        }
 
-		int row = dao.delCom(dto);
-		return row > 0;
-	}
-	
-	// COMMENT LIST
+        int row = dao.delCom(dto);
+        return row > 0;
+    }
 
-	public Map<String, Object> comList(int post_idx) {
-		
-		result = new HashMap<>();
-		
-		List<ComDTO> comList = dao.comList(post_idx);
-		int comments = dao.comCnt(post_idx);
-		
-		result.put("list", comList);
-		result.put("comments", comments);
-		
-		return result;
-	}
+    // COMMENT LIST
+    public Map<String, Object> comList(int post_idx) {
 
+        result = new HashMap<>();
+
+        List<ComDTO> comList = dao.comList(post_idx);
+        int comments = dao.comCnt(post_idx);
+
+        result.put("list", comList);
+        result.put("comments", comments);
+
+        return result;
+    }
 
 
 }
