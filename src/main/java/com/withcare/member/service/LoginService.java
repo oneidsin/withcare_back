063ca98f8bfed37f65af2aa2987ba006fa1124f7
choseohye <hyesaye@icloud.com>
2025-05-26 14:57:28 +0900

    넌 뭐냠

--- src/main/java/com/withcare/member/service/LoginService.java
+++ src/main/java/com/withcare/member/service/LoginService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.withcare.member.dao.LoginDAO;
-import com.withcare.profile.dao.ProfileDAO;
 
 
 @Service
