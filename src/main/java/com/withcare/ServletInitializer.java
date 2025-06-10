53c13f19157c425a393762e70ec9fbb285250339
oneidsin <oneidsin@naver.com>
2025-06-10 12:36:40 +0900

    키 생성 추가

--- src/main/java/com/withcare/ServletInitializer.java
+++ src/main/java/com/withcare/ServletInitializer.java
@@ -3,11 +3,19 @@ package com.withcare;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.withcare.util.JwtToken;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WithCareApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        System.out.println("키 생성");
+
+        if (JwtToken.JwtUtils.getPri_key() == null) {
+            JwtToken.JwtUtils.setPri_key();
+        }
+
+        return application.sources(WithCareApplication.class);
+    }
 
 }
