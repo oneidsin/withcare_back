fa0f5dc3555eba38246c1b06357d7a241ce608ab
oneidsin <oneidsin@naver.com>
2025-06-10 12:56:05 +0900

    webconfig 수정

--- src/main/java/com/withcare/config/WebConfig.java
+++ src/main/java/com/withcare/config/WebConfig.java
@@ -21,13 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:" + absolutePath + "/");
     }
     
-    @Override
+    /*@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-    }
-    
+    }*/
 } 
